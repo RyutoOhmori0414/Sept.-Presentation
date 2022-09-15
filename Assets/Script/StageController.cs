ce9ab97b5af99e02cb96697d8fa3c0bb87582e69
RyutoOhmori <ryutoomori0414@gmail.com>
2022-09-15 23:04:00 +0900

    [add] キャラクターが攻撃対象を選べるように調整中

--- Assets/Script/StageController.cs
+++ Assets/Script/StageController.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 public class StageController : MonoBehaviour
 {
     [Tooltip("1Wave‚Ì“G"), SerializeField]
-    GameObject[] _wave1Enemies= default;
+    GameObject _wave1Enemies= default;
     [Tooltip("2Wave‚Ì“G"), SerializeField]
-    GameObject[] _wave2Enemies = default;
+    GameObject _wave2Enemies = default;
     [Tooltip("BossWave‚Ì“G"), SerializeField]
-    GameObject[] _waveBossEnemies = default;
+    GameObject _waveBossEnemies = default;
 
     void CallWave1()
     {
-
+        Instantiate(_wave1Enemies, Vector3.zero, Quaternion.identity);
     }
 
     void CallWave2()
     {
-
+        Instantiate(_wave2Enemies, Vector3.zero, Quaternion.identity);
     }
 
     void CallWaveBoss()
     {
-
+        Instantiate(_waveBossEnemies, Vector3.zero, Quaternion.identity);
     }
 }
