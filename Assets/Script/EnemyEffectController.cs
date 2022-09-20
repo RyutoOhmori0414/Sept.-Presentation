43e60ab22510e7ea5fba377594b0396328aa424a
RyutoOhmori <ryutoomori0414@gmail.com>
2022-09-21 00:32:28 +0900

    [add] Enemyの被弾エフェクトの追加

---
+++ Assets/Script/EnemyEffectController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEffectController : MonoBehaviour
+{
+    public void EffectDestroy()
+    {
+        Destroy(gameObject);
+    }
+}
